dac1264f83026e85e96739183cdc1d083d44131e
pallavi-kawade <pallavi.kawade@appliedaiconsulting.com>
2023-05-09 23:50:33 +0530

    commit

--- backend.tf
+++ backend.tf
@@ -3,16 +3,7 @@ terraform {
     key = "terraform.tfstate"
     bucket = "tf-file-store-bucket"
     region = "us-east-1"
-    #bucket = var.bucket_name
 
   }
 }
 
-
-# terraform {
-#   backend "s3" {
-#     bucket = "my-terraform-state-bucket"
-#     key    = "my-terraform-state-key"
-#     region = "us-west-2"
-#   }
-# }
