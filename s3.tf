5e691dad6ec13c71b2d16f779b790a25ec2de18d
pallavi-kawade <pallavi.kawade@appliedaiconsulting.com>
2023-05-09 22:02:39 +0530

    commit

--- s3.tf
+++ s3.tf
@@ -1,35 +1,68 @@
-# Creating S3 Bucket to store Objects
-resource "aws_s3_bucket" "create_bucket" {
-  bucket = var.bucket_name
-  force_destroy = true
-  tags = {
-    Name = format("%v-%v-s3Bucket", var.project, var.environment)
-  }
-}
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.create_bucket.id
-  acl    = "public-read"
+# # Creating S3 Bucket to store Objects
+# resource "aws_s3_bucket" "create_bucket" {
+#   bucket = var.bucket_name
+#   force_destroy = true
+#   tags = {
+#     Name = format("%v-%v-s3Bucket", var.project, var.environment)
+#   }
+# }
+# resource "aws_s3_bucket_acl" "bucket_acl" {
+#   bucket = aws_s3_bucket.create_bucket.id
+#   acl    = "public-read"
+# }
+
+# # Attaching Policy to S3 Bucket
+ resource "aws_s3_bucket_policy" "bucket_policy" {
+   bucket = aws_s3_bucket.create_bucket.id
+   policy = data.aws_iam_policy_document.bucket_policy_document.json
+ }
+
+# resource "aws_s3_object" "object" {
+
+#   bucket = aws_s3_bucket.create_bucket.id
+
+#   key = "about.html"
+
+#   source = "./about.html"
+
+#   content_type = "text/html"
+
+#   depends_on = [
+#     aws_s3_bucket.create_bucket
+#   ]
+
+# }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-example-bucket"
 }
 
-# Attaching Policy to S3 Bucket
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.create_bucket.id
-  policy = data.aws_iam_policy_document.bucket_policy_document.json
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
 }
 
 resource "aws_s3_object" "object" {
 
   bucket = aws_s3_bucket.create_bucket.id
 
-  key = "about.html"
+   key = "about.html"
 
-  source = "./about.html"
+   source = "./about.html"
 
-  content_type = "text/html"
+   content_type = "text/html"
 
-  depends_on = [
-    aws_s3_bucket.create_bucket
-  ]
+   depends_on = [
+     aws_s3_bucket.create_bucket
+   ]
 
-}
-
+} 
