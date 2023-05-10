c132bfa20de0509caaa8adf52ef07b9585b811a7
pallavi-kawade <pallavi.kawade@appliedaiconsulting.com>
2023-05-11 03:46:28 +0530

    commit

--- s3.tf
+++ s3.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "create_bucket" {
 
  resource "aws_s3_bucket_acl" "bucket_acl" {
    bucket = aws_s3_bucket.create_bucket.id
-   acl    = "public"
+   acl    = "public-read"
 
  }
 
